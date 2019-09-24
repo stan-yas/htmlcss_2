942f6e475f0871ba8a861bbcbb8bd589ad2c6ff1
Stas <stanislav.yas@gmail.com>
2019-09-24 19:27:31 +0600

    gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
+"use strict";
+
 const gulp = require("gulp");
 const less = require("gulp-less");
-gulp.task("less", function() {
+// const plumber = require("gulp-plumber");
+// const postcss = require("gulp-postcss");
+// const autoprefixer = require("autoprefixer");
+
+function style(){
   return gulp.src("./barbershop/less/style.less")
+    // .pipe(plumber())
     .pipe(less())
+    // .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest("./barbershop/css"));
-});
+}
+
+function do_watch(){
+  return gulp.watch(["./barbershop/less/*.less","./barbershop/less/blocks/*.less"],
+    {delay: 1000}, style);
+}
+
+exports.style = style;
+exports.serve = gulp.series(style, do_watch);
