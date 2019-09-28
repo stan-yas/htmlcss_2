1b2ab257ea7e6b7b6ccc93a545769dff130ff46b
Stas <stanislav.yas@gmail.com>
2019-09-28 19:28:48 +0600

    gulpfile changed another one

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,29 @@ const less = require("gulp-less");
 // const plumber = require("gulp-plumber");
 // const postcss = require("gulp-postcss");
 // const autoprefixer = require("autoprefixer");
+const server = require("browser-sync").create();
 
 function style(){
   return gulp.src("./barbershop/less/style.less")
     // .pipe(plumber())
     .pipe(less())
     // .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("./barbershop/css"));
+    .pipe(gulp.dest("./barbershop/css")); /*.pipe(server.stream()*/
 }
 
-function do_watch(){
+function watch_less(){
   return gulp.watch(["./barbershop/less/*.less","./barbershop/less/blocks/*.less"],
     {delay: 1000}, style);
 }
 
+function serve(){
+  server.init({
+    server: "./barbershop/"
+  });
+  // watch_less();
+  gulp.watch(["./barbershop/*.html", "./barbershop/css/*.css"]).on('change',server.reload);    /*, {delay: 1000}, server.reload);*/
+}
+
 exports.style = style;
-exports.serve = gulp.series(style, do_watch);
+exports.watch = watch_less;
+exports.watch_serve = gulp.parallel(watch_less, serve);
