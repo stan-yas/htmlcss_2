1837494c3ea7781ec89dbb78948292dc1c5ee1da
Stas <stanislav.yas@gmail.com>
2019-11-10 20:48:56 +0600

    graphics minimized

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,36 @@
 
 const gulp = require("gulp");
 const less = require("gulp-less");
+const gulp_imagemin = require("gulp-imagemin");
+const imagemin = require("imagemin");
+const webp = require("imagemin-webp");
 // const plumber = require("gulp-plumber");
 // const postcss = require("gulp-postcss");
 // const autoprefixer = require("autoprefixer");
 const server = require("browser-sync").create();
+
+function min_images() {
+  return gulp.src("./img/*")
+    .pipe(gulp_imagemin())
+    .pipe(gulp.dest("./img"));
+}
+
+function min_webp() {
+  return (async () => {
+    const files = await imagemin(["./img/*.jpg"], {
+      destination: "./img",
+      plugins: [
+        webp({
+          quality: 65 // Quality setting from 0 to 100
+          /*lossless: true // Losslessly encode images*/
+        })
+      ]
+    });
+    for (let i = 0; i < files.length; i++){
+      console.log(files[i].destinationPath);
+    }
+  })();
+}
 
 function style(){
   return gulp.src("./less/style.less")
@@ -31,3 +57,5 @@ function serve(){
 exports.style = style;
 exports.watch = watch_less;
 exports.watch_serve = gulp.parallel(watch_less, serve);
+exports.imagemin = min_images;
+exports.webp = min_webp;
